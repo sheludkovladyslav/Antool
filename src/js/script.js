293e11b1fdf2ccbf4132ae921893026efdc77233
sheludkovladyslav <vladsh2103@gmail.com>
2023-07-25 00:14:27 -0400

    Додав функціоналу до newcomer tools, також додав функціоналу для кнопки в секції contributor

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-document.getElementById('loadMore').addEventListener("click", function () {
+document.getElementById("loadMore").addEventListener("click", function () {
   document.getElementById("card-hidden").style.display = "flex";
   document.getElementById("hideMore").style.display = "flex";
   document.getElementById("card-hidden-notepad").style.display = "flex";
@@ -7,11 +7,30 @@ document.getElementById('loadMore').addEventListener("click", function () {
   this.style.display = "none";
 });
 
-document.getElementById('hideMore').addEventListener("click", function () {
+document.getElementById("hideMore").addEventListener("click", function () {
   document.getElementById("card-hidden").style.display = "none";
   document.getElementById("card-hidden-notepad").style.display = "none";
   document.getElementById("card-hidden-atom").style.display = "none";
   document.getElementById("loadMore").style.display = "flex";
+  this.style.display = "none";
+});
+
+document.getElementById("joinUs").addEventListener("click", function () {
+  alert("Дякую за те, що ви долучились до спільноти ANTOOL.");
+});
+
+document.getElementById("exploreMore").addEventListener("click", function () {
+  document.getElementById("hidden-picsArt").style.display = "flex";
+  document.getElementById("hidden-illustrator").style.display = "flex";
+  document.getElementById("hide-more").style.display = "block";
 
   this.style.display = "none";
+});
+
+document.getElementById("hide-more").addEventListener("click", function () {
+  document.getElementById("hidden-picsArt").style.display = "none";
+  document.getElementById("hidden-illustrator").style.display = "none";
+  document.getElementById("exploreMore").style.display = "block";
+
+  this.style.display = "none"
 });
